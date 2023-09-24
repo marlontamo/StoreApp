f620dd3126a949584c6399638283e29fdc46de55
marlontamo <marlontamo@gmail.com>
2023-09-24 21:39:20 +0800

    fixed the routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\StoreController;
+use App\Models\User;
+use App\Models\Storemodel;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,12 +19,19 @@ use App\Http\Controllers\StoreController;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/test-foreignKey', function(){
+   $user = User::findOrFail(1);
+   $store = $user->store()->get();
+   return dd($store);
+});
 
 Auth::routes();
-Route::resource('store',StoreController::class);
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-//view store list
- Route::get('/store',[StoreController::class, 'index'])->name('store.list');
 
- Route::post('/store',[StoreController::class,'store'])->name('store.save');
-
+ Route::group(['middleware' =>['auth']], function(){
+    Route::resource('store',StoreController::class);
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
+    //view store list
+     Route::get('/store',[StoreController::class, 'index'])->name('store.list');
+    
+     Route::post('/store',[StoreController::class,'store'])->name('store.save');
+ });
