f0f1b090dc7446f0764c78df36a48caf0bc72db1
marlontamo2 <marlontamo2@gmail.com>
2025-03-06 13:03:40 +0800

    test api route

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,9 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+Route::get('/v1/', function(){
+    return "My API routes";
+});
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
